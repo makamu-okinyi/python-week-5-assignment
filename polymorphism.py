ab4fe15fe3948a44f9f1f3bedc6fceb3bb013712
makamu-okinyi <makamubetsy@gmail.com>
2025-08-28 09:26:01 +0300

    Update polymorphism.py

---
+++ polymorphism.py
@@ -0,0 +1,21 @@
+class Vehicle:
+    def move(self):
+        print("This vehicle moves 🚙")
+
+class Car(Vehicle):
+    def move(self):
+        print("🚗 Driving on the road")
+
+class Plane(Vehicle):
+    def move(self):
+        print("✈️ Flying in the sky")
+
+class Boat(Vehicle):
+    def move(self):
+        print("🚤 Sailing on water")
+
+# Polymorphism in action
+vehicles = [Car(), Plane(), Boat()]
+
+for v in vehicles:
+    v.move()
